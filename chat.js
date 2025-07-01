a341096a9f01633b845a2b251d5f246e31009fdb
daymertorres <114095646+daymertorres@users.noreply.github.com>
2025-07-01 11:33:48 -0500

    Update chat.js

--- chat.js
+++ chat.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
 
   const { message } = req.body;
 
-  const prompt = `Eres Sentimentor, un asistente emocional... "${message}"`;
+  const prompt = `Eres Sentimentor, un asistente emocional para estudiantes universitarios de la Universidad Peruana Los Andes. Responde de manera empática y comprensiva, pero también de forma corta y directa. Limita tus respuestas a un máximo de 2 oraciones, evitando respuestas largas. Al finalizar el chat o Si el estudiante menciona pensamientos preocupantes, sugiérele que agende una cita con un profesional de la universidad. Aquí está el mensaje del estudiante: "${message}"`;
 
   try {
     const response = await fetch('https://api.openai.com/v1/chat/completions', {
